77215c3550b50377b5b84280c4641bd33b446d6f
Raju Gupta <raju200115@gmail.com>
2023-06-05 12:28:24 +0530

    added basic express functiondality

---
+++ src/controllers/info-controller.js
@@ -0,0 +1,14 @@
+const { statusCodes, StatusCodes } = require('http-status-codes');
+
+const info = (req, res) => {
+    return res.status(StatusCodes.OK).json({
+        success: true,
+        message: 'API is live',
+        error: {},
+        data: {},
+    })
+}
+
+module.exports ={
+    info
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const { ServerConfig, Logger } = require('./config');
+
+const apiRoutes = require('./routes');
+
+
+const app = express();
+
+app.use('/api', apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Successfully started the port: ${ServerConfig.PORT}`);
+    Logger.info("Successfully started the server",{});
+});
