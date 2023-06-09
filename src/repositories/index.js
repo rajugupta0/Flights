8e19376e9c3046fa5632f83ce24ddf041644f358
rajugupta0 <raju200115@gmail.com>
2023-06-09 23:44:39 +0530

    Added create city API

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,4 +1,5 @@
 module.exports= {
-    AirplaneRepository: require('./airplane-repository')
+    AirplaneRepository: require('./airplane-repository'),
+    CityRepository: require('./city-repository')
 
 }
