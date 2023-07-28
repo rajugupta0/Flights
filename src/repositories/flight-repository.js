77bf6537edd1d246fef5d6e48e91b2adf9675ba0
rajugupta0 <raju200115@gmail.com>
2023-07-28 20:30:49 +0530

    FEAT: Added Complex Joins for Fetching all Airports

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,17 +1,43 @@
+const { Sequelize } = require('sequelize');
+
 const CrudRepository = require('./crud-repository');
-const { Flight } = require('../models');
+const { Flight, Airplane, Airport } = require('../models');
 
 class FlightRepository extends CrudRepository {
     constructor() {
         super(Flight);
     }
 
-    async getAllFlights(filter) {
-        const response = await  Flight.findAll({
-            where: filter
+    async getAllFlights(filter, sort) {
+        const response = await Flight.findAll({
+            where: filter,
+            order: sort,
+            include: [
+                {
+                    model: Airplane,
+                    required: true,
+                    as: 'airplaneDetail',
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'departureAirport',
+                    on : {
+                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+                    },
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'arrivalAirport',
+                    on : {
+                        col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+                    },
+                }
+            ]
         });
         return response;
     }
+    
 }
-
 module.exports = FlightRepository;
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -64,6 +64,7 @@ async function getAllFlights(query) {
             const flight = await flightRepository.getAllFlights(customFilter);
             return flight;
         } catch (error) {
+            console.log(error);
             throw new AppError('Cannot fetch data of all the flights', StatusCodes.INTERNAL_SERVER_ERROR);
         }
 }
