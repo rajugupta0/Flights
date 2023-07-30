cb4573151944346a50ca5e8ed753dcd55f637492
rajugupta0 <raju200115@gmail.com>
2023-07-30 11:44:00 +0530

    Added Seats model in db associated with airplane

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const CrudRepository = require('./crud-repository');
-const { Flight, Airplane, Airport } = require('../models');
+const { Flight, Airplane, Airport, City } = require('../models');
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -25,6 +25,10 @@ class FlightRepository extends CrudRepository {
                     on : {
                         col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
                     },
+                    include: {
+                        model: City,
+                        required: true
+                    }
                 },
                 {
                     model: Airport,
@@ -33,6 +37,10 @@ class FlightRepository extends CrudRepository {
                     on : {
                         col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
                     },
+                    include: {
+                        model: City,
+                        required: true
+                    }
                 }
             ]
         });
